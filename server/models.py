a2e20c909b0e56759293a4610ecb4973d85ff86f
Neeraj Suthar <n.raj.suthar@gmail.com>
2018-12-28 12:30:55 +0530

    setup login and signup mutations

--- server/models.py
+++ server/models.py
@@ -1,10 +1,18 @@
 from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
     uuid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), index=True, unique=True)
     posts = db.relationship('Post', backref='author')
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return '<User %r>' % self.username
--- server/schema.py
+++ server/schema.py
@@ -1,5 +1,6 @@
 from models import Post, User
 import graphene
+from graphql import GraphQLError
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from db import db
 
@@ -8,14 +9,46 @@ class PostObject(SQLAlchemyObjectType):
     class Meta:
         model = Post
         interfaces = (graphene.relay.Node, )
+
+
 class UserObject(SQLAlchemyObjectType):
    class Meta:
        model = User
        interfaces = (graphene.relay.Node, )
+       exclude_fields = ('password_hash')
+
+
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
     all_posts = SQLAlchemyConnectionField(PostObject)
     all_users = SQLAlchemyConnectionField(UserObject)
+
+
+class SignUp(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+    user = graphene.Field(lambda: UserObject)
+    def mutate(self, info, username, password):
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return SignUp(user=user)
+
+
+class Login(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+    user = graphene.Field(lambda: UserObject)
+    def mutate(self, info, username, password):
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            raise GraphQLError("Invalid Credentials")
+        return Login(user=user)
+
+
 class CreatePost(graphene.Mutation):
     class Arguments:
         title = graphene.String(required=True)
@@ -30,6 +63,12 @@ class CreatePost(graphene.Mutation):
         db.session.add(post)
         db.session.commit()
         return CreatePost(post=post)
+
+
 class Mutation(graphene.ObjectType):
     create_post = CreatePost.Field()
+    signup = SignUp.Field()
+    login = Login.Field()
+
+
 schema = graphene.Schema(query=Query, mutation=Mutation)
